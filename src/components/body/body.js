a0050607e412fdebcf8b5f3b12b034c58521c429
carolina.silvag <carolinasguzman@gmail.com>
2018-03-21 19:40:06 -0300

    actualizacion de react 15 para que materialize funcione

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-materialize';
+import Category from '../category/category';
+import All from '../all/all';
 
 class Body extends Component {
   constructor(prop) {
@@ -7,7 +10,17 @@ class Body extends Component {
   }
 
   render() {
-    
+    return (
+    <Row>
+      <Col s={2} m={2} className='grid-example'>
+        <Category/>
+      </Col>
+      <Col s={10} m={10} className='grid-example'>
+        
+      </Col>
+      
+    </Row>
+    )
   }
 
 }
--- src/components/category/category.js
+++ src/components/category/category.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Col, Collapsible, CollapsibleItem, Collection, CollectionItem } from 'react-materialize';
+
 
 class Category extends Component {
   constructor() {
@@ -6,7 +8,54 @@ class Category extends Component {
   }
 
   render() {
-    return 
+    return (
+      <div  className="Categories">
+        
+        <Collapsible accordion>
+          <CollapsibleItem header='Cuidado' icon='filter_drama'>
+            <Collection>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 1')}>Categoria 1</CollectionItem>
+              <CollectionItem href='#' active className="category" onClick={() => this.handleAuthor('Categoria 2')}>Categoria 2</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 3')}>Categoria 3</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 4')}>Categoria 4</CollectionItem>
+            </Collection>
+          </CollapsibleItem>
+          <CollapsibleItem header='Salud' icon='place'>
+            <Collection>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 1')}>Categoria 1</CollectionItem>
+              <CollectionItem href='#' active className="category" onClick={() => this.handleAuthor('Categoria 2')}>Categoria 2</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 3')}>Categoria 3</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 4')}>Categoria 4</CollectionItem>
+            </Collection>
+          </CollapsibleItem>
+          <CollapsibleItem header='otros' icon='place'>
+            <Collection>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 1')}>Categoria 1</CollectionItem>
+              <CollectionItem href='#' active className="category" onClick={() => this.handleAuthor('Categoria 2')}>Categoria 2</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 3')}>Categoria 3</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 4')}>Categoria 4</CollectionItem>
+            </Collection>
+          </CollapsibleItem>
+          <CollapsibleItem header='otros 2' icon='whatshot'>
+            <Collection>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 1')}>Categoria 1</CollectionItem>
+              <CollectionItem href='#' active className="category" onClick={() => this.handleAuthor('Categoria 2')}>Categoria 2</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 3')}>Categoria 3</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 4')}>Categoria 4</CollectionItem>
+            </Collection>
+          </CollapsibleItem>
+          <CollapsibleItem header='otros 3' icon='place'>
+            <Collection>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 1')}>Categoria 1</CollectionItem>
+              <CollectionItem href='#' active className="category" onClick={() => this.handleAuthor('Categoria 2')}>Categoria 2</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 3')}>Categoria 3</CollectionItem>
+              <CollectionItem href='#' className="category" onClick={() => this.handleAuthor('Categoria 4')}>Categoria 4</CollectionItem>
+            </Collection>
+          </CollapsibleItem>
+        </Collapsible>
+
+      </div>
+    );
   }
 }
 
